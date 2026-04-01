2b392d1990c2da6ce6b261b8159779c2e2cf1f12
dchinikalov <45802140+VolumeBlez@users.noreply.github.com>
2026-04-01 22:24:45 +0300

    refactor enemy script and simplify api for enemy target init

--- Assets/Scripts/Runtime/Features/Enemy/EnemiesController.cs
+++ Assets/Scripts/Runtime/Features/Enemy/EnemiesController.cs
@@ -1,15 +1,22 @@
+using Runtime.Features.Enemy.Thin;
 using UnityEngine;
 
 namespace Runtime.Features.Enemy
 {
     public class EnemiesController : MonoBehaviour
     {
-        [SerializeField] private ThinEnemyAI _thinAI;
-    
         public void Init(GameObject player)
         {
-            if (_thinAI != null)
-                _thinAI.InitPlayer(player);
+            var thinAi = FindAnyObjectByType<ThinEnemyAI>();
+
+            if (thinAi != null)
+            {
+                thinAi.InitPlayer(player);
+            }
+            else
+            {
+                Debug.LogError("No Thin AI assigned");
+            }
         }
     }
 }
