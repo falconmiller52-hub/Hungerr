e7bf75ec6afc19dd4197f247eb44d8751e1436e6
DuckHacker-0xD <psnturkey2024xd@gmail.com>
2026-04-21 15:24:07 +0300

    Add IPausble Intefrace in Player Camera and Disable Rotate in Pause
    Also move inventory in new Camera

--- Assets/Scripts/Runtime/Features/Player/Other/PlayerCamera.cs
+++ Assets/Scripts/Runtime/Features/Player/Other/PlayerCamera.cs
@@ -1,9 +1,32 @@
+using Cinemachine;
+using Runtime.Common.Services.Pause;
 using UnityEngine;
+using Zenject;
 
 namespace Runtime.Features.Player.Other
 {
-	public class PlayerCamera :  MonoBehaviour
+	public class PlayerCamera : MonoBehaviour, IPausable
 	{
-		// TODO: Отключать вращение при паузе
+		private CinemachineVirtualCamera _cinemachineVirtualCamera;
+		private IPauseController _pauseController;
+
+		[Inject]
+		private void Construct(IPauseController pauseController)
+		{
+			_pauseController = pauseController;
+		}
+
+		private void Awake()
+		{
+			_cinemachineVirtualCamera = GetComponent<CinemachineVirtualCamera>();
+
+			_pauseController.Add(this);
+		}
+
+		public void Stop()
+			=> _cinemachineVirtualCamera.enabled = false;
+
+		public void Resume()
+			=> _cinemachineVirtualCamera.enabled = true;
 	}
 }
