ef32236a3315e13a9402850ed77f3b6e8b08756e
DuckHacker-0xD <psnturkey2024xd@gmail.com>
2026-04-23 22:54:16 +0300

    script refactoring

--- Assets/Scripts/Runtime/Features/Dialog/View/DialogUiButtonSound.cs
+++ Assets/Scripts/Runtime/Features/Dialog/View/DialogUiButtonSound.cs
@@ -1,8 +1,6 @@
-using System;
 using System.Collections.Generic;
 using FMODUnity;
 using Runtime.Common.Services.Audio;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
