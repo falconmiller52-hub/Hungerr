63b1824214c567d8e64cc63ec0ec0e77f98a021b
dchinikalov <45802140+VolumeBlez@users.noreply.github.com>
2026-04-27 11:37:02 +0300

    add inventory events

--- Assets/Scripts/Runtime/Common/Services/Input/InputHandler.cs
+++ Assets/Scripts/Runtime/Common/Services/Input/InputHandler.cs
@@ -27,6 +27,9 @@ namespace Runtime.Common.Services.Input
 		public event Action DialogSkipInputPressed =  delegate { };
 		public event Action ExitInputPressed = delegate { };
 		public event Action InventoryTriggerPressed = delegate { };
+		public event Action InventoryGrabPressed = delegate { };
+		public event Action InventoryReleasePressed = delegate { };
+		public event Action InventoryUsePressed = delegate { };
 		public event Action<Vector2> PlayerMoveInputChanged = delegate { };
 		public event Action<bool> JumpInputPressed = delegate { };
 		public event Action<Vector2> RotateInputChanged = delegate { };
@@ -57,6 +60,10 @@ namespace Runtime.Common.Services.Input
 			Input.Player.Exit.performed += _ => ExitInputPressed?.Invoke();
 			
 			Input.Player.InventoryTrigger.performed += _ => InventoryTriggerPressed?.Invoke();
+			
+			Input.Inventory.Grab.performed += _ => InventoryGrabPressed?.Invoke();
+			Input.Inventory.Release.performed += _ => InventoryReleasePressed?.Invoke();
+			Input.Inventory.Use.performed += _ => InventoryUsePressed?.Invoke();
 		}
 		
 		public void Enable()
