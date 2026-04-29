e1227f0c97b28e3b168eee6b541b46781e0b07c2
DuckHacker-0xD <psnturkey2024xd@gmail.com>
2026-04-29 20:54:43 +0300

    Refinement debug gizmos enemy

--- Assets/Scripts/Runtime/Features/Enemy/EnemyDebugGizmos.cs
+++ Assets/Scripts/Runtime/Features/Enemy/EnemyDebugGizmos.cs
@@ -9,28 +9,21 @@ namespace Runtime.Features.Enemy
 		[InfoBox("Красная сфера - радиус атаки\n" +
 		         "Зеленая сфера - радиус зрения")]
 		
-		[Header("So всех врагов")] 
-		[SerializeField] private EnemySettingData _thinData;
+		[Header("So врага который спавниться")] 
+		[SerializeField] private EnemySettingData _enemySettingData;
 
 		[Header("Enemy type")] 
 		[SerializeField] private EnemyType _enemyType;
 		
 		private void OnDrawGizmosSelected()
 		{
-			switch (_enemyType)
-			{
-				case EnemyType.Thin:
-				{
-					// Радиус атаки
-					Gizmos.color = Color.red;
-					Gizmos.DrawWireSphere(transform.position, _thinData.AttackRadius);
-					
-					// Радиус обнаружения
-					Gizmos.color = Color.green;
-					Gizmos.DrawWireSphere(transform.position, _thinData.DetectionRadius);
-					break;
-				}
-			}
+			// Радиус атаки
+			Gizmos.color = Color.red;
+			Gizmos.DrawWireSphere(transform.position, _enemySettingData.AttackRadius);
+			
+			// Радиус обнаружения
+			Gizmos.color = Color.green;
+			Gizmos.DrawWireSphere(transform.position, _enemySettingData.DetectionRadius);
 		}
 	}
 }
