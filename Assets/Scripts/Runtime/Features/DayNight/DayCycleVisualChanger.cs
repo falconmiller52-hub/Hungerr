9d49d854552c653f2dcd6b9072e2dabd703e0ed9
dchinikalov <45802140+VolumeBlez@users.noreply.github.com>
2026-05-01 10:38:02 +0300

    change DayCycleVisualChanger and add some fog

--- Assets/Scripts/Runtime/Features/DayNight/DayCycleVisualChanger.cs
+++ Assets/Scripts/Runtime/Features/DayNight/DayCycleVisualChanger.cs
@@ -9,20 +9,27 @@ namespace Runtime.Features.DayNight
 	[ExecuteInEditMode]
 	public class DayCycleVisualChanger : MonoBehaviour
 	{
+		[Header(" Light Sources")]
 		[SerializeField] private Light _directionalLight;
 		[SerializeField] private Light _moon;
 		
-		[Header("Day Settings")] [SerializeField]
-		private Material _daySkybox;
-
+		[Header("Fog Colors")]
+		[SerializeField] private Color _dayFogColor;
+		[SerializeField] private Color _nightFogColor;
+		
+		[Header("Day Settings")] 
+		[SerializeField] private Material _daySkybox;
 		[SerializeField] private Color _dayLightColor;
 		[SerializeField] private Color _dayAmbientColor;
-		[SerializeField] private float _dayLightXRotation = 80f;
+		[SerializeField, Tooltip(" поворот солнца днем")] private float _sunDayXRotation = 80f;
 
 		[Header("Night Gradient Settings")] 
-		[SerializeField] private float _nightStartLightXRotation = -90f;
-		[SerializeField] private float _nightStartLightXRotationMoon = 90f;
-		[SerializeField] private float _startMoonIntensity = 4.0f;
+		[SerializeField, Tooltip("стартовый поворот солнца ночью")] private float _sunStartXRotation = -90f;
+		[SerializeField] private float _sunEndXRotation = 0f;
+		[SerializeField, Tooltip("стартовый поворот луны ночью")] private float _moonStartXRotation = 90f;
+		[SerializeField] private float _moonEndXRotation = 180f;
+		
+		[SerializeField, Tooltip("интенсивность луны в начале ночи")] private float _startMoonIntensity = 4.0f;
 
 		[SerializeField] private Material _nightSkybox;
 		[SerializeField] private Gradient _direactionalLightGraident;
@@ -49,8 +56,8 @@ namespace Runtime.Features.DayNight
 			_moon.color = _ambientLightGradient.Evaluate(timeProgress);
 			RenderSettings.ambientLight = _ambientLightGradient.Evaluate(timeProgress);
 
-			float xAngle = Mathf.Lerp(_nightStartLightXRotation, 0f, timeProgress);
-			float xMoonAngle = Mathf.Lerp(_nightStartLightXRotationMoon, 180, timeProgress);
+			float xAngle = Mathf.Lerp(_sunStartXRotation, _sunEndXRotation, timeProgress);
+			float xMoonAngle = Mathf.Lerp(_moonStartXRotation, _moonEndXRotation, timeProgress);
 			float intensity = Mathf.Lerp(_startMoonIntensity, 0f, timeProgress);
 			
 			_moon.intensity = intensity;
@@ -60,31 +67,52 @@ namespace Runtime.Features.DayNight
 
 		public void SetNight()
 		{
+			RenderSettings.fogColor = _nightFogColor;
 			RenderSettings.skybox = _nightSkybox;
 			_moon.intensity = _startMoonIntensity;
 			
 			_moon.gameObject.SetActive(true);
 			
 			Vector3 nightStartRotation = _defaultAngles;
-			nightStartRotation.x = _nightStartLightXRotation;
+			nightStartRotation.x = _sunStartXRotation;
 			_directionalLight.transform.localEulerAngles = nightStartRotation;
 
 			nightStartRotation = _defaultMoonAngles;
-			nightStartRotation.x = _nightStartLightXRotationMoon;
+			nightStartRotation.x = _moonStartXRotation;
 			_moon.transform.localEulerAngles = nightStartRotation;
 		}
-
+		
 		public void SetDay()
 		{
 			_moon.gameObject.SetActive(false);
 			
+			RenderSettings.fogColor = _dayFogColor;
 			RenderSettings.skybox = _daySkybox;
 			_directionalLight.color = _dayLightColor;
 			RenderSettings.ambientLight = _dayAmbientColor;
 
 			Vector3 dayRotation = _defaultAngles;
-			dayRotation.x = _dayLightXRotation;
+			dayRotation.x = _sunDayXRotation;
 			_directionalLight.transform.localEulerAngles = dayRotation;
+		}
+		
+		// Debug
+		
+		[ContextMenu("Set Day")]
+		public void SetDayDebug()
+		{
+			SetDay();
+		}
+
+		[ContextMenu("Set Night")]
+		public void SetNightDebug()
+		{
+			SetNight();
+			
+			_directionalLight.color = _ambientLightGradient.Evaluate(0);
+			_moon.color = _ambientLightGradient.Evaluate(0);
+			RenderSettings.ambientLight = _ambientLightGradient.Evaluate(0);
+			
 		}
 	}
 }
